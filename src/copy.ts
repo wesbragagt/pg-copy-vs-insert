43de6ce66a08bcad2d891bd035d9a9cc6de9f3af
Wes Braga <wesbragagt@gmail.com>
2024-08-20 15:34:00 -0500

    chore: use pg-copy-stream

--- src/copy.ts
+++ src/copy.ts
@@ -2,9 +2,9 @@ import fs from 'fs';
 import path from 'path';
 import { pipeline } from 'node:stream/promises';
 import { logger } from './logger';
-import postgres from 'postgres';
-import { ReadableStream } from 'stream/web';
+import pg from 'pg';
 import { Readable } from 'stream';
+import { from } from 'pg-copy-streams';
 
 export async function handleCopy() {
   const readCsvInMemory = fs.readFileSync(path.join(__dirname, 'workers.csv'), 'utf-8');
@@ -22,15 +22,14 @@ export async function handleCopy() {
     }
   }).filter(o => !!o.name);
 
-  const sql = postgres(process.env.DATABASE_URL as string, {
-    //close a connection that has either been idle for 20 seconds or existed for more than 30 minutes
-    idle_timeout: 20,
-    max_lifetime: 60 * 30
+  const client = new pg.Client({
+    connectionString: process.env.DATABASE_URL,
   });
+  const start = performance.now();
   try {
-    const start = performance.now();
+    await client.connect();
     logger.info('Copying data to the database');
-    await sql`
+    await client.query(`
       CREATE TEMP TABLE temp_workers (
         id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
         name TEXT NOT NULL,
@@ -42,34 +41,35 @@ export async function handleCopy() {
         zip TEXT NOT NULL,
         country TEXT NOT NULL
       );
-    `
-    const writeableStream = await sql`COPY temp_workers (name, email, phone, address, city, state, zip, country) FROM STDIN WITH (FORMAT csv)`.writable();
+    `)
+    const writeableStream = client.query(from(`COPY temp_workers (name, email, phone, address, city, state, zip, country) FROM STDIN WITH (FORMAT csv)`));
     const sourceStream = Readable.from([
       ...arrayOfObjects.map(o => `${o.name},${o.email},${o.phone},${o.address},${o.city},${o.state},${o.zip},${o.country}\n`)
     ])
     const totalChunks = arrayOfObjects.length;
-    let processed = 0;
 
-    await pipeline(sourceStream, async function*(sourceStream){
+    const copyingStart1 = performance.now();
+    await pipeline(sourceStream, async function*(sourceStream) {
+      let processed = 0;
       for await (const chunk of sourceStream) {
-        logger.info({
-          msg: `Copy ${processed}/${totalChunks}`,
-        });
         processed++;
+        if (processed % 10000 === 0) {
+          logger.info(`Processed ${processed} of ${totalChunks} rows`);
+        }
         yield chunk;
       }
     }, writeableStream)
-    logger.info(`Copied data to temp table in ${performance.now() - start}ms`);
+    logger.info(`Copied data to temp table in ${performance.now() - copyingStart1}ms`);
 
 
-    await sql`
+    const copyingStart2 = performance.now();
+    await client.query(`
       INSERT INTO workers (id,name, email, phone, address, city, state, zip, country)
       SELECT gen_random_uuid(),name, email, phone, address, city, state, zip, country
       FROM temp_workers
       ON CONFLICT (name) DO NOTHING
-    `
-    const end = performance.now();
-    logger.info(`Copied data to the database workers table in ${end - start}ms`);
+    `)
+    logger.info(`Copied data to the workers table in ${performance.now() - copyingStart2}ms`);
   }
   catch (error) {
     logger.error({
@@ -78,6 +78,7 @@ export async function handleCopy() {
     process.exit(1);
   }
   finally {
-    await sql.end();
+    logger.info(`Total time taken: ${performance.now() - start}ms`);
+    await client.end();
   }
 }
